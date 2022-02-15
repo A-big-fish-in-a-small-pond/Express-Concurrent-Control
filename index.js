6645d13bf9b88e2a3650e3d89b875dba8be8920e
Junho <api@sejongtelecom.net>
2022-02-15 13:35:38 +0900

    express-concurrent-control base code initialize

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { ExpressLimit } = require("./limiter");
+const app = express();
+const expressLimit = new ExpressLimit();
+
+/** middleware */
+app.use(express.json());
+app.use(expressLimit.checkLimitHandler);
+app.use("/", (req, res) => {
+    return res.json({ success: "end" });
+});
+
+app.listen(3000, "0.0.0.0", () => {
+    console.log("server open");
+});
