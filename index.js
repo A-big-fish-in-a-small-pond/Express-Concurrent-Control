e629b95a147ce409ab98bdfb8d622389f6f6fed4
Junho <api@sejongtelecom.net>
2022-02-17 14:11:30 +0900

    Added and tested not only the default repository, but also the Redis repository and custom repository.

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const limiter = expressLimit.expressLimit({
     maxPerMinute: 30,
     resetTime: 60,
     errorCodeNumber: 404,
+    type: "default", // 레디스를 사용하고자 하면 redis 를 넣어줍니다.
     handler: (req, res, next) => {
         res.json({ errorHandler: "this page is exceed request page" });
     },
 });
+limiter.setAccessStore();
 
-const conLimiter = expressLimit.createConcurrentQueue(2);
+/** 레디스를 등록하는 함수입니다. */
+const conLimiter = expressLimit.createConcurrentQueue(1);
 
 /** middle ware */
 app.use(express.json());
@@ -21,7 +24,7 @@ app.use(conLimiter.checkLimitHandler);
 app.use("/abs", (req, res) => {
     setTimeout(() => {
         res.json({ success: "end page" });
-    }, 10000);
+    }, 1);
 });
 
 app.listen(3000, "0.0.0.0", () => {
--- limit/options.js
+++ limit/options.js
@@ -3,6 +3,7 @@ const limiterOption = {
     resetTime: 60,
     errorCodeNumber: 404,
     handler: null,
+    type: "default",
 };
 
 const errorCode = {
@@ -10,5 +11,13 @@ const errorCode = {
     errorMessage: "페이지를 요청하는 도중 요청량 초과로 인해 잠시 차단되었습니다. 잠시 후 다시 요청해주시기 바랍니다.",
 };
 
+const REDIS = "redis";
+const CUSTOM = "custom";
+const DEFAULT = "default";
+
 module.exports.limiterOption = limiterOption;
 module.exports.errorCode = errorCode;
+
+module.exports.REDIS = REDIS;
+module.exports.CUSTOM = CUSTOM;
+module.exports.DEFAULT = DEFAULT;
